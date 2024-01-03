0948cafa958f7e434714d3d4e9a6c37afbd0088b
Torben Larsen <torben.larsen@deptagency.com>
2024-01-03 15:33:49 +0100

    Cleanup

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -2,7 +2,7 @@ import CSS from "./style.css.ts"
 import "apple-tv-slider"
 import { AppleTVSlider } from "apple-tv-slider"
 import { html, LitElement } from "lit"
-import { customElement, state } from "lit/decorators.js"
+import { customElement } from "lit/decorators.js"
 import { createRef, Ref, ref } from "lit/directives/ref.js"
 
 @customElement("main-element")
@@ -10,9 +10,6 @@ export class MainElement extends LitElement {
   static get styles() {
     return [CSS]
   }
-
-  // @state() private disableNext = false
-  // @state() private disablePrev = false
   slider: Ref<AppleTVSlider> = createRef()
 
   handleNext = () => {
@@ -25,11 +22,14 @@ export class MainElement extends LitElement {
   renderItems = () => {
     const items = []
     for (let i = 0; i < 10; i++) {
-        items.push(html`        
-            <slider-item>
-                <img style="width: 100%; object-fit: cover;" src="https://picsum.photos/600/400?${i}" />
-            </slider-item>
-        `)
+      items.push(html`
+        <slider-item>
+          <img
+            style="width: 100%; object-fit: cover;"
+            src="https://picsum.photos/600/400?${i}"
+          />
+        </slider-item>
+      `)
     }
     return items
   }
@@ -38,7 +38,7 @@ export class MainElement extends LitElement {
     return html`
       <section class="wrapper">
         <appletv-slider speed="900ms" ${ref(this.slider)}>
-            ${this.renderItems()}
+          ${this.renderItems()}
         </appletv-slider>
         <div class="buttons">
           <button class="button" @click="${this.handlePrev}">Prev</button>
