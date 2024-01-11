56baf001a4b73dc4d7ac80bab57bbac71e088fad
Torben Larsen <torben.larsen@deptagency.com>
2024-01-11 14:11:11 +0100

    Clean up

--- src/apple-tv-slider.ts
+++ src/apple-tv-slider.ts
@@ -1,28 +1,42 @@
-import { html, LitElement, PropertyValueMap } from 'lit'
-import { customElement, property, queryAssignedElements, state } from 'lit/decorators.js'
-import CSS from './apple-tv-slider.css';
-import classNames from 'classnames';
-import './apple-tv-slider-item';
-
+import { html, LitElement, css } from "lit"
+import {
+  customElement,
+  property,
+  queryAssignedElements,
+  state,
+} from "lit/decorators.js"
+import classNames from "classnames"
+import "./apple-tv-slider-item"
 
 /**
  * Slider in the style of Apple TV
- * 
+ *
  * @element appletv-slider
  * @slot The <slide-item> slides *
  * @attribute speed - The speed of the slide transition
  * @fires change - When the active slide changes
- * 
+ *
  */
 
-@customElement('appletv-slider')
+@customElement("appletv-slider")
 export class AppleTVSlider extends LitElement {
-  static get styles() { return [ CSS ] }
-  
-  @property({ type: String }) speed = '1000ms'
-  @state() active = 0
-  @queryAssignedElements({}) listItems!: Array<HTMLElement>;
+  static styles = css`
+    :host {
+      display: block;
+      overflow: hidden;
+      --slider-speed: 1000ms;
+    }
 
+    .container {
+      display: grid;
+      grid-template-areas: "all";
+      height: 100%;
+    }
+  `
+
+  @property({ type: String }) speed = "1000ms"
+  @state() active = 0
+  @queryAssignedElements({}) listItems!: Array<HTMLElement>
 
   protected firstUpdated(): void {
     this.updateClasses()
@@ -35,19 +49,23 @@ export class AppleTVSlider extends LitElement {
   private updateClasses = () => {
     this.listItems.forEach((item, i) => {
       const c = classNames({
-        'active': this.active === i,
-        'after': i > this.active,
-        'before': i < this.active
-      });
+        active: this.active === i,
+        after: i > this.active,
+        before: i < this.active,
+      })
       item.className = c
     })
-    this.dispatchEvent(new CustomEvent('change', { detail: {active: this.active} }))
+    this.dispatchEvent(
+      new CustomEvent("change", { detail: { active: this.active } })
+    )
   }
 
   private get _next() {
-    return this.active === this.listItems.length-1 ? this.active : this.active + 1
+    return this.active === this.listItems.length - 1
+      ? this.active
+      : this.active + 1
   }
-  
+
   private get _prev() {
     return this.active === 0 ? 0 : this.active - 1
   }
@@ -55,7 +73,7 @@ export class AppleTVSlider extends LitElement {
   public next = () => {
     this.active = this._next
   }
-  
+
   public prev = () => {
     this.active = this._prev
   }
@@ -63,7 +81,7 @@ export class AppleTVSlider extends LitElement {
   render() {
     return html`
       <div class="container" style="--slider-speed: ${this.speed}">
-          <slot></slot>
+        <slot></slot>
       </div>
     `
   }
@@ -71,6 +89,6 @@ export class AppleTVSlider extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'appletv-slider': AppleTVSlider
+    "appletv-slider": AppleTVSlider
   }
 }
