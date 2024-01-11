17ed3d441e250d9f198d0db96a66d3c112b0b878
Torben Larsen <torben.larsen@deptagency.com>
2024-01-11 14:59:39 +0100

    Added Storybook

---
+++ src/stories/AppleTvSlider.stories.ts
@@ -0,0 +1,75 @@
+import type { Meta, StoryObj } from "@storybook/web-components"
+import { html, LitElement } from "lit"
+import "../apple-tv-slider"
+import { AppleTVSlider } from "../apple-tv-slider"
+
+const meta: Meta = {
+  title: "AppleTV Slider",
+  component: "appletv-slider",
+  parameters: {
+    docs: {
+      description: {
+        // 👈 The description of the component
+        component: "Description…",
+      },
+    },
+  },
+
+  argTypes: {},
+}
+export default meta
+
+const renderItems = () => {
+  const items = []
+  for (let i = 0; i < 10; i++) {
+    items.push(html`
+      <slider-item>
+        <img
+          style="width: 100%; height: 100%; position: absolute; object-fit: cover;"
+          src="https://picsum.photos/1600/900?${i}"
+        />
+      </slider-item>
+    `)
+  }
+  return items
+}
+
+export const Default: StoryObj<AppleTVSlider> = {
+  args: {},
+  render: () => {
+    return html`
+      <section class="wrapper">
+        <appletv-slider speed="900ms"> ${renderItems()} </appletv-slider>
+        <div class="buttons">
+          <button
+            class="button"
+            @click="${() => document.querySelector("appletv-slider")?.prev()}"
+          >
+            Prev
+          </button>
+          <button
+            class="button"
+            @click="${() => document.querySelector("appletv-slider")?.next()}"
+          >
+            Next
+          </button>
+        </div>
+      </section>
+      <style>
+        appletv-slider {
+          height: 900px;
+        }
+        .buttons {
+          display: flex;
+          justify-content: space-between;
+          margin: 10px 0;
+          padding-bottom: 50px;
+        }
+        .button {
+          border: 1px solid #ccc;
+          padding: 5px 20px;
+        }
+      </style>
+    `
+  },
+}
